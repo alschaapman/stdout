dc9d0c945567acbe356176c55f029dcba3f934fa
Al Schaapman <me@alschaapman.com>
2019-11-19 05:14:10 -0800

    Move index.html out of ignored dir

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = process.env.HTTP_PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.listen(port, () => {});
