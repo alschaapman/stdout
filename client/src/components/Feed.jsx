0ccf9e56e5578f71d8b5ac579a38a81aebb2a42d
Al Schaapman <me@alschaapman.com>
2019-11-19 05:45:44 -0800

    Create Feed component

---
+++ client/src/components/Feed.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Post from './Post.jsx';
+
+class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Feed;
